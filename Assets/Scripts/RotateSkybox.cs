7631f5ddcc7bee7b309f2d42fe327b9ab3f05db3
CAHenry <craig.henry@imperial.ac.uk>
2020-02-17 11:50:51 +0000

    resposition screens and skybox

---
+++ Assets/Scripts/RotateSkybox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSkybox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", -270);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
